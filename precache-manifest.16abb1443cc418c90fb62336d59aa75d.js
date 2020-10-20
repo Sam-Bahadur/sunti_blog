ce6b09a774be12ccd50406152dc674c1924acde3
Pradeep K <pradipktimsina@gmail.com>
2020-10-20 11:46:32 +0545

    Updates

---
+++ precache-manifest.16abb1443cc418c90fb62336d59aa75d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc22ab33e3c53d963636d06890cad31",
+    "url": "/sunti_blog/index.html"
+  },
+  {
+    "revision": "75d96daf1704efcbb291",
+    "url": "/sunti_blog/static/css/main.735afb25.chunk.css"
+  },
+  {
+    "revision": "387a6ce6fee44f3717dd",
+    "url": "/sunti_blog/static/js/2.2c071ab7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sunti_blog/static/js/2.2c071ab7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75d96daf1704efcbb291",
+    "url": "/sunti_blog/static/js/main.c70c93c3.chunk.js"
+  },
+  {
+    "revision": "40bece6c4e3e45013edf",
+    "url": "/sunti_blog/static/js/runtime-main.20aa5d02.js"
+  },
+  {
+    "revision": "cd5c9d539776b779a0f0499aa98e7499",
+    "url": "/sunti_blog/static/media/hero.cd5c9d53.JPG"
+  },
+  {
+    "revision": "c40babe6b4ca1baa6bda230b731e791d",
+    "url": "/sunti_blog/static/media/logo.c40babe6.png"
+  }
+]);
